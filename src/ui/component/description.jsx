d8a368e202ce7e266d1b5238ce5f0f094904ca4a
TEJAS-SHINDEE <tejasshindee2024@gmail.com>
2025-01-28 23:28:14 +0530

    fixed Desc

--- src/ui/component/description.jsx
+++ src/ui/component/description.jsx
@@ -3,33 +3,28 @@ import React from "react";
 const Description = () => {
 	return (
 		<>
-			<div id="description" className="description  h-screen flex flex-col lg:flex lg:flex-row justify-between">
-					<section className=" info xl:w-[70%] relative pb-10 w-full flex flex.col gap-4 ">
-						<div className="flex flex-col gap-2">
-						<span className="font-medium text-lg ">
-							Hi, I'm
-						</span>
-						<h1 className="font-bold text-xl md:text-7xl">
-							TEJAS SHINDE
-						</h1>
-						</div>
-						<div className="relative">
-							<h4 className="font-semibold text-xl m-2">Software Engineer</h4>
-							<p className=" mt-2 ">
-								I specialize in building WebApps using React, Next.js, and
-								TypeScript, turning designs into engaging web experiences.
-							</p>
-						</div>
-					</section>
+			<div id="description" className="description  lg:h-screen flex flex-col lg:flex lg:flex-row justify-between">
 
-					<div className="photo md:w-[40%] md:flex md:items-center md:justify-center ">
-						<img
-							className="h-96 w-96 object-cover"
-							src="./assets/photo.png"
-							alt=""
-						/>
+				<div className=" info xl:w-[70%] relative w-full flex flex-col ">
+					<span className="font-medium text-lg">Hi, I'm</span>
+					<h1 className="font-bold text-6xl md:text-7xl ">TEJAS SHINDE</h1>
+					<div className="relative">
+						<button className="font-semibold text-xl bg-slate-300 p-2 rounded-lg">Software Engineer</button>
+						<p className=" mt-2 ">
+							I specialize in building WebApps using React, Next.js, and
+							TypeScript, turning designs into engaging web experiences.
+						</p>
 					</div>
 				</div>
+
+				<div className="photo w-[80%] md:w-[30%] md:flex md:items-center md:justify-center ">
+					<img
+						className="h-full w-full object-cover"
+						src="./assets/photo.png"
+						alt=""
+					/>
+				</div>
+			</div>
 		</>
 	);
 };
--- src/ui/component/header.jsx
+++ src/ui/component/header.jsx
@@ -47,29 +47,33 @@ export const Header = () => {
           </li>
         </nav>
 
-        {/* Mobile Menu */}
-        {menuOpen && (
-          <ul className="flex flex-row items-start gap-4 mt-2 p-4  shadow-lg lg:hidden">
-            <li className="hover:underline">
-              <a href="#about">ABOUT</a>
-            </li>
-            <li className="hover:underline">
-              <a href="#description">EXPERIENCE'S</a>
-            </li>
-            <li className="hover:underline">
-              <a href="##">WORKS</a>
-            </li>
-            <li className="hover:underline">
-              <a href="##">CONTACT</a>
-            </li>
-            <li className="hover:underline border-gray-500">
-              <a href="#contact">
-                VIEW RESUME <i className="ri-file-text-fill">{" "}</i>
-              </a>
-            </li>
-          </ul>
-        )}
+        
       </div>
+
+      {/* Mobile Menu */}
+      {menuOpen && (
+          <div className="z-40 bg-green-200 h-full">
+            <ul className="flex flex-col items-start gap-4 mt-2 p-4 h-full shadow-lg lg:hidden">
+              <li className="hover:underline">
+                <a href="#about">ABOUT</a>
+              </li>
+              <li className="hover:underline">
+                <a href="#description">EXPERIENCE'S</a>
+              </li>
+              <li className="hover:underline">
+                <a href="##">WORKS</a>
+              </li>
+              <li className="hover:underline">
+                <a href="##">CONTACT</a>
+              </li>
+              <li className="hover:underline border-gray-500">
+                <a href="#contact">
+                  VIEW RESUME <i className="ri-file-text-fill">{" "}</i>
+                </a>
+              </li>
+            </ul>
+          </div>
+        )}
     </>
   );
 };
